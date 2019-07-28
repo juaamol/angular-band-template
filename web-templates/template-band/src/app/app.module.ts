43d8e0560c0e263d12341ff7033526a8ef3135d6
juaamol <jambulud@gmail.com>
2019-07-28 20:26:37 +0200

    updated

--- web-templates/template-band/src/app/app.module.ts
+++ web-templates/template-band/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavbarComponent } from './src/components/navbar/navbar.component';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { NavbarComponent } from './src/components/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
