564f55720d3ea00bf38ae2eedc1475061526a3b8
juaamol <jambulud@gmail.com>
2019-08-03 15:24:39 +0200

    booking table finished

--- web-templates/template-band/src/app/app.module.ts
+++ web-templates/template-band/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { MatMenuModule } from '@angular/material/menu';
-import { CarouselComponent } from './components/carousel/carousel.component';
-import { CarouselItemDirective } from './components/carousel/directives/carousel-item/carousel-item.directive';
-import { CarouselItemElement } from './components/carousel/directives/carousel-item-element/carousel-item-element.directive';
-import { BandMemberComponent } from './components/band-member/band-member.component';
-import { SectionComponent } from './components/section/section.component';
-import { BookingInfoComponent } from './components/booking-info/booking-info.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { MatMenuModule } from "@angular/material/menu";
+import { CarouselComponent } from "./components/carousel/carousel.component";
+import { CarouselItemDirective } from "./components/carousel/directives/carousel-item/carousel-item.directive";
+import { CarouselItemElement } from "./components/carousel/directives/carousel-item-element/carousel-item-element.directive";
+import { BandMemberComponent } from "./components/band-member/band-member.component";
+import { SectionComponent } from "./components/section/section.component";
+import { BookingInfoComponent } from "./components/booking-info/booking-info.component";
+import { MatTableModule } from "@angular/material/table";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatRippleModule } from "@angular/material/core";
 
 @NgModule({
   declarations: [
@@ -31,9 +34,12 @@ import { BookingInfoComponent } from './components/booking-info/booking-info.com
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatMenuModule
+    MatMenuModule,
+    MatTableModule,
+    MatChipsModule,
+    MatRippleModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- web-templates/template-band/src/app/components/booking-info/booking-info.component.ts
+++ web-templates/template-band/src/app/components/booking-info/booking-info.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+export interface Booking {
+  status: string;
+  month: string;
+  soldout: boolean;
+  numTickets: number;
+}
+
+const BOOKING_DATA: Booking[] = [
+  { month: "September", status: 'Sold out', soldout: true, numTickets: 0 },
+  { month: "October", status: 'Sold out', soldout: true, numTickets: 0 },
+  { month: "November", status: 'Sold out', soldout: false, numTickets: 3 },
+];
 
 @Component({
-  selector: 'app-booking-info',
-  templateUrl: './booking-info.component.html',
-  styleUrls: ['./booking-info.component.css']
+  selector: "app-booking-info",
+  templateUrl: "./booking-info.component.html",
+  styleUrls: ["./booking-info.component.css"],
 })
 export class BookingInfoComponent implements OnInit {
+  displayedColumns: string[] = ["month", "status", "numTickets"];
+  dataSource = BOOKING_DATA;
+  rippleColor = 'rgba(171, 166, 166, 0.5)';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
